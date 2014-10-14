8633fd6b4050545e0d6cbed9ae5424a49bb938e2
Fumio SAGAWA <fumio.sagawa@gmail.com>
2014-10-14 17:30:57 +0900

    develにサンプル追加

--- client/app/fuga/fuga.controller.js
+++ client/app/fuga/fuga.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('angularRouteApp')
-  .controller('FugaCtrl', function ($scope) {
+  .controller('FugaCtrl', function ($scope, $location) {
     $scope.message = 'Hello';
+
+    console.log('aaaaaa');
+
+    $scope.submit = function(form) {
+      
+      console.log('form ok');
+
+      console.log('ひとつめのディレクトリ:'+$scope.id1);
+      console.log('ふたつめのディレクトリ:'+$scope.id2);
+
+      if(form.$valid) {
+      	console.log($scope.userType);
+        // サービスを起動する
+        // .successでurlを飛ばす
+        $location.path($scope.id1+$scope.id2+'/hoge');
+      }
+    }
   });
